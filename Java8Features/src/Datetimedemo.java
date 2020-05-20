32d86bb8604c499f809c652291e5848e33dfe8cf
SaiViswanthTunuguntla <57610731+SaiViswanthTunuguntla@users.noreply.github.com>
2020-05-19 22:21:35 -0500

    localdatetime demo

---
+++ Java8Features/src/Datetimedemo.java
@@ -0,0 +1,8 @@
+import java.time.LocalDateTime;
+
+public class Datetimedemo {
+    public static void main(String[] args) {
+        LocalDateTime dt= LocalDateTime.now();
+        System.out.println(dt);
+    }
+}
